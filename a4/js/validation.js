41a85431a775aec15c891f1233cd6dcb6712796f
sam-wade-bc <sam.wade@bellevuecollege.edu>
2025-06-02 23:26:20 -0700

    Update validation.js

--- a4/js/validation.js
+++ a4/js/validation.js
@@ -1,11 +1,35 @@
 function validateForm() {
     
-// Set validity variable
+// Set validity variable to start
     var isValid = true;
 
-// Clear previous messages
-    var errorMessagesDiv = document.getElementById("errorMessages");
-    errorMessagesDiv.innerHTML = "";
+// establish each validation ids
+var invalidDiv = document.getElementById("invalidDiv")
+var firstNameInvalidDiv = document.getElementById("firstNameInvalidDiv");
+var lastNameInvalidDiv = document.getElementById("lastNameInvalidDiv");
+var emailInvalidDiv = document.getElementById("emailInvalidDiv");
+var phoneInvalidDiv = document.getElementById("phoneInvalidDiv");
+var usernameInvalidDiv  = document.getElementById("usernameInvalidDiv");
+var passwordInvalidDiv = document.getElementById("passwordInvalidDiv");
+var addressInvalidDiv = document.getElementById("addressInvalidDiv");
+var stateInvalidDiv = document.getElementById("stateInvalidDiv");
+var cityInvalidDiv = document.getElementById("cityInvalidDiv");
+var countryInvalidDiv = document.getElementById("countryInvalidDiv");
+var zipCodeInvalidDiv = document.getElementById("zipCodeInvalidDiv");
+
+// Set all error messages clear first
+    invalidDiv.innerHTML = "";
+    firstNameInvalidDiv.innerHTML = "";
+    lastNameInvalidDiv.innerHTML = "";
+    emailInvalidDiv.innerHTML = "";
+    phoneInvalidDiv.innerHTML = "";
+    usernameInvalidDiv.innerHTML = "";
+    passwordInvalidDiv.innerHTML = "";
+    addressInvalidDiv.innerHTML = "";
+    cityInvalidDiv.innerHTML="";
+    stateInvalidDiv.innerHTML = "";
+    zipCodeInvalidDiv.innerHTML = "";
+    
 
 // Establish variables to validate
     var firstName = document.getElementById("FirstName").value.trim();
@@ -14,7 +38,7 @@ function validateForm() {
     var phone = document.getElementById("Phone").value.trim();
     var username = document.getElementById("Username").value.trim();
     var password = document.getElementById("Password").value.trim();
-    var address = document.getElementById("Address").value.trim();
+    var address = document.getElementById("Address").value;
     var city = document.getElementById("City").value.trim();
     var state = document.getElementById("State").value.trim();
     var country = document.getElementById("Country").value;
@@ -23,66 +47,81 @@ function validateForm() {
 
 //Validate first name
     if (firstName === "" || firstName.length > 20) {
-        errorMessagesDiv.innerHTML += "<p>Please enter your First Name (max length: 20).</p>";
+        firstNameInvalidDiv.innerHTML += "<p>Please enter your First Name (max length: 20).</p>";
         isValid = false;
     }
 
 //Validate Last Name
     if (lastName === "" || lastName.length > 20) {
-        errorMessagesDiv.innerHTML += "<p>Please enter your Last Name (max length: 20).</p>";
+        lastNameInvalidDiv.innerHTML += "<p>Please enter your Last Name (max length: 20).</p>";
         isValid = false;
     }
 
 //Establish email regular expression then validate
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (email === "" || !emailRegex.test(email)) {
-        errorMessagesDiv.innerHTML += "<p>Please enter a valid Email address.</p>";
+        emailInvalidDiv.innerHTML += "<p>Please enter a valid Email address.</p>";
         isValid = false;
     }
 //Establish phone regular expression then validate
     var phoneRegex = /^[0-9]+$/;
     if (phone === "" || !phoneRegex.test(phone)) {
-        errorMessagesDiv.innerHTML += "<p>Please enter a valid Phone number (numeric only).</p>";
+        phoneInvalidDiv.innerHTML = "<p>Please enter a valid Phone number (numeric only).</p>";
         isValid = false;
     }
 
 //Validate username
-
+if (username.length>12|| username===""){
+     usernameInvalidDiv.innerHTML = "<p>Invalid username, Maximum 12 characters Allowed</p>";
+     isValid=false;
+}
 
 //Validate password
     var hasUpperCase = /[A-Z]/;
     var hasLowerCase = /[a-z]/;
     var hasNumber = /[0-9]/;
     var hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/;
-    var 
-    if (!hasUpperCase.test(password)&& !hasLowerCase.test(password)&& !hasNumber.test(password)&& !hasSpecialChar(password)){
-     errorMessagesDiv.innerHTML += "<p>Password is invalid, Please include one of each: Upper Case, Lower Case, Number, and Special Character.</p>";
+    if (!hasUpperCase.test(password)&& !hasLowerCase.test(password)&& !hasNumber.test(password)&& !hasSpecialChar.test(password)){
+     passwordInvalidDiv.innerHTML = "<p>Password is invalid, Please include one of each: Upper Case, Lower Case, Number, and Special Character.</p>";
      isValid=false;
     }
 
-//Validate address with length
+//Validate address with length or blank
     if (address === "" || address.length > 50) {
-        errorMessagesDiv.innerHTML += "<p>Please enter your Address (max length: 50).</p>";
+        addressInvalidDiv.innerHTML = "<p>Please enter your Address (max length: 50).</p>";
         isValid = false;
     }
 
+//Validate city
+
+if (city === ""||city.length>15){
+  cityInvalidDiv.innerHTML="<p>Invlaid City (Max characters:15).</p>";
+  isValid = false;
+}
+
 //Validate state only if country is USA
     if (country==="001" && state === "000"){
-       errorMessagedDiv.innerHTML += "<p>Please Select a State</P>";
+       stateInvalidDiv.innerHTML = "<p>Please Select a State</P>";
        isValid = false;
     }
 
 //Validate country
     if (country === "000") {
-        errorMessagesDiv.innerHTML += "<p>Please select a valid Country.</p>";
+        countryInvalidDiv.innerHTML = "<p>Please select a valid Country.</p>";
         isValid = false;
     }
 //Establish zip code regular exression then validate 
-    var zipRegex = /^[0-5]+$/;
+    var zipRegex = /^\d{5}$/;
     if (zipCode===""||!zipRegex.test(zipCode)){
-       errorMessagesDiv.innerHTML += "<p>Please enter a valid zip code. (Max length: 5)</p>"
+       zipCodeInvalidDiv.innerHTML = "<p>Please enter a valid zip code. (Max length: 5)</p>"
        isValid = false;  
     }
+  
+//Set error if the form is invalidated in any way
+if (!isValid) {
+  invalidDiv.innerHTML = "<p>Please correct the above errors before submitting again</p>";
+}
+
 //Finaly return validity status to form. 
     return isValid;
 }

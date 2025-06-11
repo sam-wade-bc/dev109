048c9da9530183eaa6c6cd349db7788a2bdd65cc
sam-wade-bc <sam.wade@bellevuecollege.edu>
2025-06-11 14:54:16 -0700

    Update carousel.js

--- a5/js/carousel.js
+++ a5/js/carousel.js
@@ -1,67 +1,89 @@
-var slideIndex = 1; // Start with the first slide
-var timerDuration = 10; // Duration for each slide in seconds
-var timerInterval; // Variable to hold the interval for the timer
+var myImages =
+["https://sam-wade-bc.github.io/dev109/a5/img/pic1.jpg",
+ "https://sam-wade-bc.github.io/dev109/a5/img/pic2.jpg",
+ "https://sam-wade-bc.github.io/dev109/a5/img/pic3.jpg",
+ "https://sam-wade-bc.github.io/dev109/a5/img/pic4.jpg",
+ "https://sam-wade-bc.github.io/dev109/a5/img/pic5.jpg"];
 
-showSlides(slideIndex);
-startTimer(timerDuration);
 
-// Function to show the slide at a specific index
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
+var captions =
+["Gato Fight: This cat has a sword, how menacing!",
+"Gato Love: This cat looks onward with admiration, cute!",
+"Gato Explore: Cats are kings of the outdoors, they love to wander and explore!",
+"Gato Climb: Cats like to climb stairs, just like this guy!",
+"Gato Suspicious: I cant describe the emotion on this cats face, he seems suspicios."];
 
-  // Handle wrapping around the slide index
-  if (n > slides.length) {
-    slideIndex = 1; // Wrap around to the first slide
+var altText = 
+["Cat with a sword",
+ "Cat with heterochromia",
+ "Cat outside",
+ "Cat on stairs",
+ "Cat on a on a ledge"];
+
+ var index=0; 
+ var timerDisplay = document.getElementById('timer');
+ var timerDuration = 4000;
+ var timerInterval;
+ var nextButton = document.getElementById("next"); 
+ var previousButton = document.getElementById("previous"); 
+ var countdown = timerDuration/1000;
+ var auto = document.getElementById("auto");
+document.getElementById("auto").addEventListener("change", autoSlide);
+
+ function updateImage(){
+ document.getElementById("slideshow").src = myImages[index];
+ document.getElementById("slideshow").alt= altText[index];
+ document.getElementById("caption").textContent = captions[index]; 
+} 
+
+function next(){
+ if (auto.checked)
+ startTimer();
+ if (myImages.length == index+1)
+ index=0;
+ else
+ index++;
+ updateImage();
+} 
+ 
+
+function back(){
+ if (auto.checked)
+ startTimer();
+ if (index===0)
+ index=myImages.length-1;
+ else
+ index--;
+ updateImage();
+} 
+
+
+previousButton.addEventListener("click",back,false);
+nextButton.addEventListener("click",next,false); 
+
+function autoSlide() {
+  if (document.getElementById("auto").checked) {
+    next(); // Go to next slide when checked
+    startTimer(); // Start the timer
+  } else {
+    stopTimer(); // Stop the timer when checkbox is unchecked
   }
-  if (n < 1) {
-    slideIndex = slides.length; // Wrap around to the last slide
-  }
-
-  // Hide all slides
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  // Show the current slide
-  slides[slideIndex - 1].style.display = "block";
-
-  // Restart the timer for the new slide
-  resetTimer();
 }
 
-// Function to navigate forward or backward
-function progressPic(n) {
-  showSlides(slideIndex += n);
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerDisplay.textContent = "Automatic Slides Paused";
 }
 
-// Function to start the automatic slide advancement
-function autoSlides() {
-  slideIndex++;
-  showSlides(slideIndex);
-  setTimeout(autoSlides, timerDuration * 1000); // Change image every 10 seconds
-}
-
-// Function to start the timer
-function startTimer(duration) {
-  var timer = duration;
-  var timerDisplay = document.getElementById('timer');
-
-  timerInterval = setInterval(function() {
-    timerDisplay.textContent = 'Next slide in: ' + timer + 's';
-    timer--;
-
-    if (timer < 0) {
-      clearInterval(timerInterval);
+function startTimer() {
+  clearInterval(timerInterval);
+  var duration = countdown; 
+  timerInterval = setInterval(function () {
+    timerDisplay.textContent = "Next slide in: " + duration + "s";
+    duration--; // Decrement the duration
+    if (duration < 0) {
+      duration = countdown; // Reset the countdown after it reaches 0
+      next(); // Move to the next slide
     }
   }, 1000);
 }
-
-// Function to reset the timer
-function resetTimer() {
-  clearInterval(timerInterval);
-  startTimer(timerDuration);
-}
-
-// Start automatic slide advancement
-autoSlides();
